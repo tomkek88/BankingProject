51fce09249632729def81540e43529b50b8f7027
Tomasz Brazkiewicz <tom.brazkiewicz@gmail.com>
2017-11-27 14:30:27 +0100

    Dodano klasy: -	SavingsTransaction -	Appointment -	Recipient

---
+++ BankingProject/src/main/java/com/creatium/domain/Appointment.java
@@ -0,0 +1,52 @@
+package com.creatium.domain;
+
+public class Appointment {
+
+	private Long id;
+	private String description;
+	private String location;
+	private boolean confirmed;
+
+	private User user;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+	public boolean isConfirmed() {
+		return confirmed;
+	}
+
+	public void setConfirmed(boolean confirmed) {
+		this.confirmed = confirmed;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+}
